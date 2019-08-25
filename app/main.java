308b52a79deeb2a55621260de7ff0d7df5ab743d
dexstroy <matej.rus7@gmail.com>
2019-08-25 20:40:10 +0200

    Fix with main file

--- app/main.java
+++ app/main.java
@@ -1,4 +1,8 @@
 
-public static void main(String[] args){
-	System.out.println("Hello world");
+public class MainClass{
+
+	public static void main(String[] args){
+		System.out.println("Hello world");
+	}
+	
 }
